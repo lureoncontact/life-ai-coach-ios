5c1b9cf761086395d7a1ae5ff84f370fb3cd96e8
lureoncontact <lureoncontact@gmail.com>
2025-10-02 10:41:43 -0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,11 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.cbc8dc99333f4608b8715b1e58580f86',
   appName: 'life-ai-coach-ios',
   webDir: 'dist',
-  server: {
-    url: 'https://cbc8dc99-333f-4608-b871-5b1e58580f86.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+  // Comentar el servidor para usar build local
+  // server: {
+  //   url: 'https://cbc8dc99-333f-4608-b871-5b1e58580f86.lovableproject.com?forceHideBadge=true',
+  //   cleartext: true
+  // },
   ios: {
     contentInset: 'automatic'
   }
